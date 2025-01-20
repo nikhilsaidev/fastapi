2e274c10b173ac483874a12e128adf1ebd65427f
nikhilsaidev <nikhilsaidev@gmail.com>
2025-01-20 14:12:53 +0530

    master script to run all 4 kafka listners in a single container

---
+++ master_script_for_4_listners.py
@@ -0,0 +1,29 @@
+from multiprocessing import Process
+import listener1
+import listener2
+import listener3
+import listener4
+
+if _name_ == "_main_":
+    processes = [
+        Process(target=listener1.run),
+        Process(target=listener2.run),
+        Process(target=listener3.run),
+        Process(target=listener4.run),
+    ]
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+        
+        
+        
+#DOCKER FILE
+
+# FROM python:3.9-slim
+# WORKDIR /app
+# COPY . /app
+# RUN pip install -r requirements.txt
+# CMD ["python", "master_script.py"]
